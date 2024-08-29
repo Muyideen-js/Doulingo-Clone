c594dbdc7c98f6008a641712f7b2d789865e4229
Muyideen.jsx <yusufabolaji2007@gmail.com>
2024-08-29 15:25:03 +0100

    working on drizzle studio

---
+++ drizzle.config.ts
@@ -0,0 +1,21 @@
+// import "dotenv/config";
+// import type { Config } from 'drizzle-kit';
+
+// export default {
+//     schema: "./db/schema.ts",
+//     out: './drizzle',
+//     driver: "pglite",
+//     dbCredentials: {
+//         url: process.env.DATABASE_URL!,
+//     },
+// } satisfies Config;
+
+import { defineConfig } from "drizzle-kit";
+export default defineConfig({
+  dialect: "postgresql", 
+  schema: "./db/schema.ts",
+  out: "./drizzle",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
